3fc59e898295bd0c6380aa6eef8514ab55d6beea
Vivek Pandey <52068103+insidetheradar@users.noreply.github.com>
2020-05-10 10:11:39 +0530

    Added Dice Image
    Seeing randomly generated numbers isn't as fun as seeing a dice in the app.

--- app/src/main/java/com/insidetheradar/diceroller/MainActivity.java
+++ app/src/main/java/com/insidetheradar/diceroller/MainActivity.java
@@ -2,6 +2,7 @@ package com.insidetheradar.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton = (Button) findViewById(R.id.roll_button);
         rollButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), getString(R.string.ToastMessage), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), getString(R.string.ToastMessage), Toast.LENGTH_SHORT).show();
                 rolldice();
             }
         });
@@ -30,9 +31,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void rolldice() {
-        TextView randomNumber = findViewById(R.id.random_number);
+
         final int num = new Random().nextInt(6) + 1;
-        String number = String.valueOf(num);
-        randomNumber.setText(number);
+        ImageView diceImage = findViewById(R.id.random_number);
+
+        final int imageSelection;
+        if(num==1){
+            imageSelection = R.drawable.dice_1;
+        }
+        else if(num==2){
+            imageSelection = R.drawable.dice_2;
+        }
+        else if(num==3){
+            imageSelection = R.drawable.dice_3;
+        }
+        else if(num==4){
+            imageSelection = R.drawable.dice_4;
+        }
+        else if(num==5){
+            imageSelection = R.drawable.dice_5;
+        }
+        else{
+            imageSelection = R.drawable.dice_6;
+        }
+
+        diceImage.setImageResource(imageSelection);
+
     }
 }    
